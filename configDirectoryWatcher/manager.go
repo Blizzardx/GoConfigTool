75600a29f577c220eee931b211a07c8357a0e3cd
Baoxue-home <Blizzard_xc@126.com>
2018-07-11 23:50:48 +0800

    update

--- configDirectoryWatcher/manager.go
+++ configDirectoryWatcher/manager.go
@@ -2,6 +2,8 @@ package configDirectoryWatcher
 
 import (
 	"github.com/Blizzardx/GoConfigTool/common"
+	"io/ioutil"
+	"log"
 	"sync"
 )
 
@@ -14,19 +16,19 @@ var currentLoadingFileMap = &sync.Map{}
 //文件处理器池子 主要用来限制消费者数量
 var fileLoaderPool = common.NewSyncQueue()
 
-//文件加载器同时工作的最大数量
-const maxLoaderCount = 100
-
 //当前的版本库
 var currentVersionInfo = &sync.Map{}
 
 //需要变更刷新版本库的文件列表
 var needUpdateVersionFileQueue = common.NewSyncQueue()
 
-//目标文件的所在目录
+//input 文件加载器同时工作的最大数量
+var maxLoaderCount = 100
+
+//input 目标文件的所在目录
 var workspaceDirectory string
 
-//输出的版本索引文件名称
+//input 输出的版本索引文件名称
 var versionConfigOutputName = "version.xml"
 
 type simpleFileInfo struct {
@@ -35,7 +37,19 @@ type simpleFileInfo struct {
 	fileSign string
 }
 
-func init() {
+func Init(workspace string, versionConfigName string, maxFileLoaderCount int) {
+	if maxFileLoaderCount > 0 && maxFileLoaderCount < 100 {
+		maxLoaderCount = maxFileLoaderCount
+	}
+	if versionConfigName != "" {
+		versionConfigOutputName = versionConfigName
+	}
+	if workspace == "" {
+		log.Fatal("error workspace,workspace cannot be empty ")
+		return
+	}
+	workspaceDirectory = workspace
+
 	for i := 0; i < maxLoaderCount; i++ {
 		offerFileLoader()
 	}
