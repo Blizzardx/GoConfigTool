1e3331b7462b9c94b7e1a0dee9a8cb351b26ee6e
baoxue <blizzard_xc@126.com>
2018-07-11 16:15:33 +0800

    update

--- example/testMain.go
+++ example/testMain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/Blizzardx/GoConfigTool/configManager"
 	"github.com/Blizzardx/GoConfigTool/decoder"
+	"github.com/Blizzardx/GoConfigTool/example/auto"
 	_ "github.com/Blizzardx/GoConfigTool/example/auto"
 	"log"
 	"time"
@@ -14,5 +15,9 @@ func main() {
 	configManager.Init("example/config", "version.cfg", new(decoder.MsgPackDecodeC))
 
 	log.Println("xxxx2")
+
+	config := configManager.GetConfig("WorldPlayerInfo").(*auto.WorldPlayerInfo)
+	log.Printf("%v", config)
+
 	time.Sleep(1 * time.Minute)
 }
