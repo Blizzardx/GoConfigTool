1e1a5ed554709ced5b40b2120c835d6be755ad2f
Baoxue-home <Blizzard_xc@126.com>
2018-07-23 23:54:59 +0800

    update

---
+++ excelConfigParserTool/z_example/bin/run.bat
@@ -0,0 +1,52 @@
+cd config
+set configPath=%cd%
+
+cd ../classDefine
+set classDefinePath=%cd%
+
+cd ../windows
+1_genConfigProvision.exe %configPath% %classDefinePath% pb config
+
+@IF %ERRORLEVEL% NEQ 0 pause
+
+cd ../protobufTool
+
+set GO_OUTDIR="go/"
+
+rd /s/q %GO_OUTDIR%
+mkdir %GO_OUTDIR%
+
+protoc.exe --plugin=protoc-gen-go=protoc-gen-go.exe --go_out %GO_OUTDIR% --proto_path  %classDefinePath% %classDefinePath%/*.proto
+
+@IF %ERRORLEVEL% NEQ 0 pause
+
+cd ../import/config
+set parserOutputPath=%cd%
+
+cd ../../windows
+2_genConfigParser.exe %configPath% %parserOutputPath% pb config "github.com/Blizzardx/GoConfigTool/excelConfigParserTool/z_example/bin/import/config"
+
+@IF %ERRORLEVEL% NEQ 0 pause
+
+cd ../protobufTool/go
+set copyGoPath=%cd%
+
+echo begin copy...
+::拷贝刚刚生成的go文件
+copy *.go %parserOutputPath%
+cd ../../
+
+
+set GOARCH=amd64
+set GOOS=windows
+
+set CURR=%cd%
+cd ..\..\..\..\..\..\..\
+
+set GOPATH=%cd%
+cd %CURR%
+
+go build -o windows/import.exe github.com/Blizzardx/GoConfigTool/excelConfigParserTool/z_example/bin/import
+
+
+@IF %ERRORLEVEL% NEQ 0 pause
